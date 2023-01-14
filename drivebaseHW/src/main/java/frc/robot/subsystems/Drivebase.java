8fc6adb3cd7cda8fa19a0062c4bcf5c6808ad1d8
John Truong <itsmehelp02@gmail.com>
2023-01-13 18:52:48 -0800

    create and invert motors

--- drivebaseHW/src/main/java/frc/robot/subsystems/Drivebase.java
+++ drivebaseHW/src/main/java/frc/robot/subsystems/Drivebase.java
@@ -8,18 +8,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControlle;
 
 public class Drivebase extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public Drivebase() {}
 
-  CANSparkMax m_leftTopMotor = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax m_leftMiddleMotor = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax m_leftBottomMotor = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax m_rightTopMotor = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax m_rightMiddleMotor = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax m_rightBottomMotor = new CANSparkMax(0, MotorType.kBrushless);
+  CANSparkMax m_leftFrontMotor = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax m_leftMiddleMotor = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax m_leftBackMotor = new CANSparkMax(5, MotorType.kBrushless);
+  CANSparkMax m_rightFrontMotor = new CANSparkMax(0, MotorType.kBrushless);
+  CANSparkMax m_rightMiddleMotor = new CANSparkMax(2, MotorType.kBrushless);
+  CANSparkMax m_rightBackMotor = new CANSparkMax(4, MotorType.kBrushless);
   
+  //why?
+  m_leftFrontMotor.setInverted(true);
+  m_leftMiddleMotor.setInverted(true);
+  m_leftBackMotor.setInverted(true);
+  m_rightFrontMotor.setInverted(true);
+  m_rightMiddleMotor.setInverted(true);
+  m_rightBackMotor.setInverted(true); 
+
+  DifferentialDrive m_db = new DifferentialDrive();
 
   @Override
   public void periodic() {
